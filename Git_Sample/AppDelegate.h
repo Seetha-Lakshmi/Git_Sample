bf99974fdcaaead54156025b8235520c836a5e28
seethalakshmi <seethalakshmia@techaffinity.com>
2015-08-12 13:54:38 +0530

    Initial Commit

---
+++ Git_Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git_Sample
+//
+//  Created by Seethalakshmi.A on 12/08/15.
+//  Copyright (c) 2015 Seethalakshmi.A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git_Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git_Sample
+//
+//  Created by Seethalakshmi.A on 12/08/15.
+//  Copyright (c) 2015 Seethalakshmi.A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
